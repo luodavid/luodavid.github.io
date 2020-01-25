2621451a48580219ab9c4a05fd7913d5b0e6b028
David Luo <davidluo@berkeley.edu>
2020-01-24 23:52:25 -0800

    Kosan

---
+++ kosan/js/functions.js
@@ -0,0 +1,40 @@
+// var origWidth = document.getElementById("smallmenu").style.width;
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("smallmenu").style.width = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("smallmenu").style.width = origWidth;
+}
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  });
+
+  $('.back-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll /4 +'%)'
+  });
+
+  $('.fore-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+  });
+
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+
+    $('.clothes-pics figure').each(function(i){
+
+      setTimeout(function(){
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+
+  }
+
+});
